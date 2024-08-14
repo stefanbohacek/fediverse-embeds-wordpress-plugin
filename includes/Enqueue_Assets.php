ec15a3645f23d5db738219f99c5dab872256cf6a
Stefan Bohacek <stefan@stefanbohacek.com>
2024-08-14 10:10:45 -0400

    Move stylesheet to the footer.

--- includes/Enqueue_Assets.php
+++ includes/Enqueue_Assets.php
@@ -1,18 +1,22 @@
 <?php
+
 namespace FTF_Fediverse_Embeds;
 // require_once __DIR__ . '/../vendor/autoload.php';
-if (!class_exists('simple_html_dom_node')){
+if (!class_exists('simple_html_dom_node')) {
     require_once __DIR__ . '/../vendor/simplehtmldom/simplehtmldom/simple_html_dom.php';
 }
 
-class Enqueue_Assets {
-    function __construct(){
+class Enqueue_Assets
+{
+    function __construct()
+    {
         add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
+        add_action('wp_footer', array($this, 'enqueue_styles'));
         add_action('script_loader_tag', array($this, 'add_type_attribute'), 10, 3);
     }
 
-    function enqueue_scripts(){
-        $include_bootstrap_styles = get_option('ftf_fediverse_embeds_include_bootstrap_styles', 'on');
+    function enqueue_scripts()
+    {
         $theme = get_option('ftf_fediverse_embeds_theme', 'on');
         $show_metrics = get_option('ftf_fediverse_embeds_show_metrics', 'on');
         $show_post_labels = get_option('ftf_fediverse_embeds_show_post_labels', 'on');
@@ -26,7 +30,7 @@ class Enqueue_Assets {
 
         $plugin_data = get_file_data(__DIR__ . '/../index.php', array('Version' => 'Version'), false);
         $plugin_version = $plugin_data['Version'];
-        
+
         wp_register_script('ftf-fediverse-embeds-frontend-js', $js_url, array(), filemtime($js_path), array(
             'strategy'  => 'defer',
             'in_footer' => true
@@ -41,14 +45,21 @@ class Enqueue_Assets {
                 'theme' => $theme,
                 'show_metrics' => $show_metrics === 'on',
                 'show_post_labels' => $show_post_labels === 'on',
-                'deleted_posts' => $deleted_posts 
+                'deleted_posts' => $deleted_posts
             ),
             'version' => $plugin_version
         ));
 
         wp_enqueue_script('ftf-fediverse-embeds-frontend-js');
+    }
 
-        if ($include_bootstrap_styles === 'on'){
+    function enqueue_styles()
+    {
+        $include_bootstrap_styles = get_option('ftf_fediverse_embeds_include_bootstrap_styles', 'on');
+        $plugin_dir_url = plugin_dir_url(__FILE__);
+        $plugin_dir_path = plugin_dir_path(__FILE__);
+
+        if ($include_bootstrap_styles === 'on') {
             $css_url = $plugin_dir_url . '../dist/css/styles-bs.min.css';
             $css_path = $plugin_dir_path . '../dist/css/styles-bs.min.css';
         } else {
@@ -59,7 +70,9 @@ class Enqueue_Assets {
         wp_enqueue_style('ftf-fediverse-embeds-frontend-styles', $css_url, array(), filemtime($css_path));
     }
 
-    function add_type_attribute($tag, $handle, $src){
+
+    function add_type_attribute($tag, $handle, $src)
+    {
         if ('ftf-fediverse-embeds-frontend-js' !== $handle) {
             return $tag;
         }
